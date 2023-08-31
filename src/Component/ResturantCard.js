7a3c1113fa5d1419baae47e3c9b865ba10297289
Deepak <deepaknnayak34@gmail.com>
2023-09-01 00:49:39 +0530

    Swiggy Clone

--- src/Component/ResturantCard.js
+++ src/Component/ResturantCard.js
@@ -4,10 +4,10 @@ const RestaurantCard = (props) =>{
         <>
             <div className="resturant_card">
                 <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resdata.info.cloudinaryImageId}/>
-                <p>{resdata.info.name}</p>
-                <p>Rating: {resdata.info.avgRating}</p>
-                <p>{resdata.info.cuisines.join(", ")}</p>
-                <p>{resdata.info.areaName}</p>
+                <p className="food_name">{resdata.info.name}</p>
+                <p className="rating">Rating: {props.resdata.info.avgRating}</p>
+                <p className="cuisine_name">{resdata.info.cuisines.join(", ")}</p>
+                <p className="area_name">{resdata.info.areaName}</p>
             </div>
         </>
     )
