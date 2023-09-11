5ce73f05935f6509203350d0c720b32c7bb581ec
Deepak <deepaknnayak34@gmail.com>
2023-09-12 02:31:56 +0530

    Updated

--- src/Component/ResturantCard.js
+++ src/Component/ResturantCard.js
@@ -1,3 +1,5 @@
+import star from '../Image/star.png'
+
 const RestaurantCard = (props) =>{
     const {data} = props
     return(
@@ -5,7 +7,10 @@ const RestaurantCard = (props) =>{
             <div className="resturant_card">
                 <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + data.info.cloudinaryImageId}/>
                 <p className="food_name">{data.info.name}</p>
-                <p className="rating">{data.info.avgRating}</p>
+                <p className="rating">
+                    <img className="rating_image" src={star} />
+                    {data.info.avgRating}
+                </p>
                 <p className="cuisine_name">{data.info.cuisines.join(", ")}</p>
                 <p className="area_name">{data.info.areaName}</p>
             </div>
