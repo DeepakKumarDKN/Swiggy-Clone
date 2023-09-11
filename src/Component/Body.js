73e66460ef78dd10b9339e6949c9acd7a2f0ba3e
Deepak <deepaknnayak34@gmail.com>
2023-09-11 21:29:56 +0530

    updated

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,11 +1,24 @@
+import { useState } from "react"
+import { resList } from "../utils/Resturants"
+import RestaurantCard from "./ResturantCard"
+
+
+
 const Body = () => {
+
+    const [ListofRestaurants, setListofRestaurants] = useState(resList)
+    
     return (
         <div className="Body maxWidth">
             <div className="search_bar">
                 Search
             </div>
             <div className="resturant_container">
-                This is Restaurant Container
+                {
+                    ListofRestaurants.map((restaurant)=> (
+                        <RestaurantCard data = {restaurant}/>
+                    ))
+                }
             </div>
         </div>
     )
