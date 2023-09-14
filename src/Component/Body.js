0f6bbba3dd92e16ffdd7df998560e54df640ea80
Deepak <deepaknnayak34@gmail.com>
2023-09-15 01:21:20 +0530

    code added

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,39 +1,55 @@
 import { useEffect, useState } from "react"
-import { resList } from "../utils/Resturants"
 import RestaurantCard from "./ResturantCard"
+import { API_LINK } from "../utils/constant"
 
 
 
 const Body = () => {
 
-    let [ListofRestaurants, setListofRestaurants] = useState([])
+    const [ListofRestaurants, setListofRestaurants] = useState([])
+    const [filteredRestaurants, setfilteredRestaurants] = useState([])
+    const [searchText, setsearchText] = useState([])
 
     useEffect(()=> {
         fetchData();
     }, [])
 
+    
+
     const fetchData = async() => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4717414&lng=88.3454871&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const data = await fetch(API_LINK)
         const json = await data.json()
-        setListofRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+
+        let checkedData = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        setListofRestaurants(checkedData)
+        setfilteredRestaurants(checkedData)
     }
     
     return (
         <div className="Body maxWidth">
             <div className="buttons_section">
                 <div className="search_bar">
-                    <input type="text" placeholder="Search"/>
-                    <button type="submit" className="submit_btn">Submit</button>
+                    <input type="text" 
+                    placeholder="Search Your Favourite Restaurants Here" 
+                    value = {searchText}
+                    onChange = {(e)=> {setsearchText(e.target.value)}}
+                    />
+                    <button type="submit" className="submit_btn"
+                    onClick= {()=> {
+                        setfilteredRestaurants(ListofRestaurants.filter
+                            ((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase())))
+                    }}
+                    >Search</button>
                 </div>
                 <button className="top_res" onClick={() =>{
-                    setListofRestaurants(ListofRestaurants.filter((res)=> (res?.info?.avgRating >= 4.6)
+                    setfilteredRestaurants(ListofRestaurants.filter((res)=> (res?.info?.avgRating >= 4.6)
                     ))}}
                 >Top Resturant</button>
             </div>
 
             <div className="resturant_container">
                 {
-                    ListofRestaurants.map((restaurant)=> (
+                    filteredRestaurants.map((restaurant)=> (
                         <RestaurantCard 
                         key = {restaurant.info.id} 
                         data = {restaurant}
