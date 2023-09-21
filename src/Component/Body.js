5dbbf934991e0582c025fa7646e59b39547ac226
Deepak <deepaknnayak34@gmail.com>
2023-09-21 14:27:58 +0530

    updated

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -5,56 +5,10 @@ import { API_LINK } from "../utils/constant"
 
 
 const Body = () => {
-
-    const [ListofRestaurants, setListofRestaurants] = useState([])
-    const [filteredRestaurants, setfilteredRestaurants] = useState([])
-    const [searchText, setsearchText] = useState([])
-
-    useEffect(()=> {
-        fetchData();
-    }, [])
-
-    
-
-    const fetchData = async() => {
-        const data = await fetch(API_LINK)
-        const json = await data.json()
-
-        let checkedData = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-        setListofRestaurants(checkedData)
-        setfilteredRestaurants(checkedData)
-    }
-    
     return (
         <div className="Body maxWidth">
-            <div className="buttons_section">
-                <div className="search_bar">
-                    <input type="text" 
-                    placeholder="Search Your Favourite Restaurants Here" 
-                    value = {searchText}
-                    onChange = {(e)=> {setsearchText(e.target.value)}}
-                    />
-                    <button type="submit" className="submit_btn"
-                    onClick= {()=> {
-                        setfilteredRestaurants(ListofRestaurants.filter
-                            ((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase())))
-                    }}
-                    >Search</button>
-                </div>
-                <button className="top_res" onClick={() =>{
-                    setfilteredRestaurants(ListofRestaurants.filter((res)=> (res?.info?.avgRating >= 4.6)
-                    ))}}
-                >Top Resturant</button>
-            </div>
-
-            <div className="resturant_container">
+           <div className="resturant_container">
                 {
-                    filteredRestaurants.map((restaurant)=> (
-                        <RestaurantCard 
-                        key = {restaurant.info.id} 
-                        data = {restaurant}
-                        />
-                    ))
                 }
             </div>
         </div>
